b7834831f0953740bd8c1e1e1278a6a731b0df36
viotbery <1732844301@qq.com>
2022-08-21 18:36:24 +0800

    feat: 新增图床迁移功能

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 from tkinter import *
 from tkinter import filedialog
 from transform import transformRun
+import threading
 #@author: viotbery
 #@date: 2022-08-20
 #@description: 对markdown文件中插入图片的格式进行中转换
 #markdown文件中插入图片的格式是：![图片注释][图片序号]，因此需要将其转换为：![图片注释](图片链接)
 
 window = Tk()
-window.geometry('500x500')
+window.geometry('500x450')
 window.geometry('+700+250')
 window.title('markdown图片格式语法转换器')
 window.resizable(False, False)
@@ -39,12 +40,32 @@ outFilePathSelect = Button(window,text="选择文件夹",font=("微软雅黑",10
 
 info = Text(window,font=("微软雅黑",10),height=5,width=50,spacing3=5, padx=20, pady=20)
 
+v = IntVar()
+v.set(0)
+subLink = Entry(window,font=("微软雅黑",10), width=35)
+subLink.config(state=DISABLED)
+def select():
+    if v.get():
+        subLink.config(state=NORMAL)
+    else:
+        subLink.config(state=DISABLED)
+        
+
+Label(window, text="是否更换图床地址").grid(row=5, column=0, padx=0, pady=0, sticky=W)
+Radiobutton(window, text="是",variable=v, value=1, command = select).grid(row=5, column=1, padx=0, sticky=W)
+Radiobutton(window, text="否", variable=v, value=0, command = select).grid(row=5, column=2, padx=0, sticky=W)
+Label(window, text="图床链接前缀").grid(row=6, column=0, padx=0, pady=0, sticky=W)
+
+
 # 添加滚动条
 scroll = Scrollbar()
 
+def threadRun(func, *args):
+    threading.Thread(target=func, args=args).start()
+
 scroll.config(command=info.yview)
 info.config(yscrollcommand=scroll.set)
-run = Button(window,text="开始转换",font=("微软雅黑",10), height=1, width=10, command=lambda: transformRun(srcFilePathEntry.get(), outFilePathEntry.get(),info))
+run = Button(window,text="开始转换",font=("微软雅黑",10), height=1, width=10, command=lambda: threadRun(transformRun, srcFilePathEntry.get(), outFilePathEntry.get(),info, v.get(), subLink.get()))
 
 srcFilePath.grid(row=0,column=0)
 srcFilePathEntry.grid(row=1,column=0)
@@ -58,6 +79,8 @@ outFilePathSelect.grid(row=3,column=1, pady=10)
 
 info.grid(row=4,column=0, columnspan=3, padx=15)
 scroll.grid(row=4,column=4,sticky=N+S)
-run.grid(row=5,column=1, pady=5)
+
+subLink.grid(row=6, column=1, padx=0, sticky=W, columnspan=2)
+run.grid(row=7,column=1, pady=5)
 
 window.mainloop()
--- src/transform.py
+++ src/transform.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+from urllib import request
 import regex as re
 import os
 from tkinter import *
+import requests
 # 匹配文档中图片插入的序号，正则表达式(?<=!\[.*?\]\[)\d*(?=\])
 # 匹配图片链接注释的正则：(?<=\[\d*\]:\s+).*
-def transform(src, out, info):
+def transform(src, out, info, subLink):
     with open(src, 'r', encoding='utf-8') as f:
         lines = f.readlines()
         # 反向读取，从最后一行开始读取
@@ -19,7 +21,10 @@ def transform(src, out, info):
             if link:
                 # 若匹配到则将该链接按照其序号存入字典中
                 index = re.findall(r'(?<=^\[)\d*(?=\])', line)[0]
-                links[str(index)] = link[0]
+                r = requests.get(link[0])
+                fileName = r.headers['Content-Disposition'].split("'")[-1]
+                links[str(index)] = subLink + fileName
+                info.insert(END, '成功修改链接：%s\n' % (subLink + fileName))
                 # 并将该行删除
                 lines[i] = ''
             # 匹配文档中的插入图片的序号，如![1]
@@ -36,7 +41,10 @@ def transform(src, out, info):
             f.writelines(lines)
         return links
 
-def transformRun(src, out, info):
+def transformRun(src, out, info, * links):
+    subLink = None
+    if links[0]:
+        subLink = links[1]
     if not (src and out):
         info.insert(END, '错误:文件路径不能为空！\n')
         return
@@ -45,11 +53,11 @@ def transformRun(src, out, info):
             for file in files:
                 if file.endswith('.md'):
                     info.insert(END, '开始处理文件：%s\n' % file)
-                    transform(root + '/' + file, out, info)
+                    transform(root + '/' + file, out, info, subLink)
     if os.path.isfile(src):
-        transform(src, out, info)
+        transform(src, out, info, subLink)
     info.insert(END, '处理完成！\n')
 
 if __name__ == '__main__':
     info = Text(width=50, height=10)
-    transformRun('src/input', 'src/out', info)
+    transformRun('src/input', 'src/out', info, True, 'https://markdown-img-1311240910.cos.ap-nanjing.myqcloud.com/')
